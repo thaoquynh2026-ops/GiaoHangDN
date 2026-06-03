a3dba2a634e14d75d23c71ae17ca96d147ffe7e8
thaoquynh2026-ops <thaoquynh2026@gmail.com>
2026-06-03 12:07:15 +0700

    Add PWA support, marketing templates, QR printer

---
+++ sw.js
@@ -0,0 +1,37 @@
+const CACHE = 'ghn-v1';
+const ASSETS = [
+  '/GiaoHangDN/',
+  '/GiaoHangDN/index.html',
+  '/GiaoHangDN/admin.html',
+  '/GiaoHangDN/driver.html',
+  '/GiaoHangDN/app.js',
+  '/GiaoHangDN/style.css',
+];
+
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)).catch(()=>{}));
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(caches.keys().then(keys =>
+    Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+  ));
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', e => {
+  if (e.request.method !== 'GET') return;
+  e.respondWith(
+    caches.match(e.request).then(cached => {
+      const fresh = fetch(e.request).then(res => {
+        if (res.ok) {
+          const clone = res.clone();
+          caches.open(CACHE).then(c => c.put(e.request, clone));
+        }
+        return res;
+      }).catch(() => cached);
+      return cached || fresh;
+    })
+  );
+});
