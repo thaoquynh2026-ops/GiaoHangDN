b4e2eab95cb1a5492fe513369e80f1835830ecdb
thaoquynh2026-ops <thaoquynh2026@gmail.com>
2026-06-03 14:42:06 +0700

    Fix PWA: correct paths, install banner, improved manifest

--- sw.js
+++ sw.js
@@ -1,22 +1,29 @@
-const CACHE = 'ghn-v1';
+const CACHE = 'ghn-v3';
 const ASSETS = [
-  '/GiaoHangDN/',
-  '/GiaoHangDN/index.html',
-  '/GiaoHangDN/admin.html',
-  '/GiaoHangDN/driver.html',
-  '/GiaoHangDN/app.js',
-  '/GiaoHangDN/style.css',
+  './',
+  './index.html',
+  './admin.html',
+  './driver.html',
+  './app.js',
+  './style.css',
+  './manifest.json',
+  './icon-192.png',
+  './icon-512.png',
 ];
 
 self.addEventListener('install', e => {
-  e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)).catch(()=>{}));
+  e.waitUntil(
+    caches.open(CACHE).then(c => c.addAll(ASSETS)).catch(() => {})
+  );
   self.skipWaiting();
 });
 
 self.addEventListener('activate', e => {
-  e.waitUntil(caches.keys().then(keys =>
-    Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
-  ));
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+    )
+  );
   self.clients.claim();
 });
 
@@ -25,9 +32,8 @@ self.addEventListener('fetch', e => {
   e.respondWith(
     caches.match(e.request).then(cached => {
       const fresh = fetch(e.request).then(res => {
-        if (res.ok) {
-          const clone = res.clone();
-          caches.open(CACHE).then(c => c.put(e.request, clone));
+        if (res.ok && res.type !== 'opaque') {
+          caches.open(CACHE).then(c => c.put(e.request, res.clone()));
         }
         return res;
       }).catch(() => cached);
